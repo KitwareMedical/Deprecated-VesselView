791a326698ffaea72d422dfd0001ebc97b5e28f7
Johan Andruejol <johan.andruejol@kitware.com>
2014-06-10 18:37:42 -0400

    BUMP: Bump TubeTK to have the spatial object fixes

--- CMake/Superbuild/External_TubeTK.cmake
+++ CMake/Superbuild/External_TubeTK.cmake
@@ -45,7 +45,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${${proj}_DIR}
     GIT_REPOSITORY "https://github.com/KitwareMedical/TubeTK.git"
-    GIT_TAG "09c9f66c67b02bef46f20ae4f50d0da46bdb0a23"
+    GIT_TAG "3f74380ade5a73b4029b9c6812eadfa915748320"
     CMAKE_CACHE_ARGS
       -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
       -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
