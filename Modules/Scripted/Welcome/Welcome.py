3ea4e7633921e8d33e2fe0418fd8f7396c2faa98
Matthieu Heitz <matthieu.heitz@kitware.com>
2015-02-06 08:59:51 -0500

    Null-check on the widget list before accessing it

--- Modules/Scripted/Welcome/Welcome.py
+++ Modules/Scripted/Welcome/Welcome.py
@@ -45,7 +45,9 @@ class WelcomeWidget(ScriptedLoadableModuleWidget):
     self.onModuleSelected()
 
     # Collapse the data probe button by default
-    slicer.util.findChildren(text='Data Probe')[0].collapsed = True
+    dataProbeCollapsibleButton = slicer.util.findChildren(text='Data Probe')
+    if dataProbeCollapsibleButton is not None:
+        dataProbeCollapsibleButton[0].collapsed = True
 
   def selectWelcomeModule(self):
     slicer.util.selectModule('Welcome')
