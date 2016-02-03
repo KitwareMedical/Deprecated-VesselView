11a9b0eb463a0f428c9d3a864bfb19bba1c3fb9a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-02-03 18:04:23 -0500

    STYLE: Change "Kitware Inc." to "Kitware, Inc." and fix copyright references

--- CMake/ExternalProjectDependency.cmake
+++ CMake/ExternalProjectDependency.cmake
@@ -1,11 +1,14 @@
-################################################################################
+###########################################################################
 #
-#  Program: 3D Slicer
+#  Library:   CTK
 #
-#  Copyright (c) Kitware Inc.
+#  Copyright (c) Kitware, Inc.
 #
-#  See COPYRIGHT.txt
-#  or http://www.slicer.org/copyright/copyright.txt for details.
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0.txt
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,10 +16,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-#  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
-#  and was partially funded by NIH grant 3P41RR013218-12S1
-#
-################################################################################
+###########################################################################
 
 include(CMakeParseArguments)
 
