d84cb8987b471ed18daa08b68147fda67860bc53
Sumedha Singla <sumedha.singla@kitware.com>
2016-06-29 16:25:24 -0400

    ENH: Updated the git tag for TubeTK

--- CMake/Superbuild/External_TubeTK.cmake
+++ CMake/Superbuild/External_TubeTK.cmake
@@ -29,7 +29,7 @@ set(_source_dir ${CMAKE_BINARY_DIR}/${proj})
 ExternalProject_Add(${proj}
   ${${proj}_EP_ARGS}
   GIT_REPOSITORY "${git_protocol}://github.com/KitwareMedical/TubeTK.git"
-  GIT_TAG "9d14ee3b202c7165ca876e560e67cb1f1524cc6d"
+  GIT_TAG "28258ad181fbe940198e1ac3b490f55950320d0e"
   CONFIGURE_COMMAND ""
   PREFIX ${CMAKE_BINARY_DIR}/${proj}-prefix
   SOURCE_DIR ${_source_dir}
